a75733a813db62c6ea669757419ce6fcf03b268a
Helior <65434511+X0Fury@users.noreply.github.com>
2021-04-07 22:46:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 # -*- coding: utf-8 -*-
-import urllib.request
-import mysql.connector
-from mysql.connector import errorcode
 import requests
-import json
 import telepot
-import random
-import re
-import sys
-import time
-import random
-from subprocess import run
-from os import system
-from time import sleep
 R = '\033[1;31m'
 B = '\033[1;34m'
 C = '\033[1;37m'
 Y = '\033[1;33m'
 G = '\033[1;32m'
 RT = '\033[;0m'
-
-system('clear')
-from random import choice
 telepot == 9.1
 requests == 2.11
 json == 18.2
@@ -36,12 +21,11 @@ def handle(msg):
         print(f'''[{G}i{RT}] Uma Mensagem Que Nao E Um Texto Foi Recebida''')
         return
     command = msg['text'].split(' ')
-    if command[0] == '/menu2': # COMMAND HERE
-       bot.sendMessage(chat_id, 'text here')
+    if command[0] == '/menu': # COMANDO AQUI
+       bot.sendMessage(chat_id, 'Texto Aqui')
         
 bot = telepot.Bot('')  # YOUR TOKEN HERE
 bot.message_loop(handle)
-print(f'[{Y}i{RT}]Aguarde...')
 print(f'[{G}+{RT}]ON...')
 
 while 1:
